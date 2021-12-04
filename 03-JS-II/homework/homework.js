46c0fcba1d3389a13a36397b5c2838efdb32f794
JUNIOR-LEMUEL <93356079+JUNIOR-LEMUEL@users.noreply.github.com>
2021-12-03 22:48:27 -0500

    Solución Final TEST JSII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -137,15 +137,15 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
-    if(num1 > num2 && num1 > num3 && num1 > 0) {
-      return "Número 1 es mayor y positivo";
-    } else if(num1 < 0 || num2 < 0 || num3 < 0) {
+    if(num1 < 0 || num2 < 0 || num3 < 0) {
       return "Hay negativos";
-    } else if(num3 > num1 && num3 > num2) {
-      return num3 + 1;
-    } else if(num1 === 0 || num2 === 0 || num3 === 0) {
+    }else if(num1 === 0 || num2 === 0 || num3 === 0) {
       return "Error";
-    } else {
+    }else if(num1 > 0 && num1 > num2 && num1 > num3) {
+      return "Número 1 es mayor y positivo";
+    }else if(num3 > num1 && num3 > num2) {
+      return num3 + 1;
+    }else {
       return false;
     }
   }
@@ -184,11 +184,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-    var array = [];
-    for(var i = 0; i <= 60; i++) {
-      array.push(i * 6);
+    let arrayTab6 = [];
+    for(let i = 0; i < 11; i++){
+      arrayTab6.push(6 * i);
     }
-    return array;
+    return arrayTab6;
   }
 
 
